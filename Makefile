8f1ea593632b1582910d62264d9f350b9386b83e
Endre Farkas <farkas.endre511@gmail.com>
2026-03-24 15:39:22 +0200

    Step 0 complete

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 CC = gcc
-OBJ = main.o helper.o
+OBJ = main.o
 LIBS = -l csfml-graphics -l csfml-system -l csfml-audio -l csfml-window
 PRG_N = Chess
 
-$(PRG_N) : $(OBJ)
+$(PRG_N): $(OBJ)
 	$(CC) -Wall -g -o $(PRG_N) $(OBJ) $(LIBS)
 
-$(OBJ) : helper.h
+$(OBJ):
 
-.PHONY : clean
-clean :
+.PHONY: clean
+clean:
 	-rm prg $(OBJ)
--- main.c
+++ main.c
@@ -5,13 +5,27 @@
 
 int main(int argc, char* argv[])
 {
-    sfRenderWindow* window = sfRenderWindow_create((sfVideoMode){400, 400, 8}, "Chess: The Game", 1 << 2, NULL);
+    sfRenderWindow* window = sfRenderWindow_create((sfVideoMode){400, 400, 8}, "Chess: The Game", 0x4, NULL);
+
+    sfEvent event;
 
     while (sfRenderWindow_isOpen(window))
     {
+        while (sfRenderWindow_pollEvent(window, &event))
+        {
+            if (event.type == sfEvtClosed)
+            {
+                sfRenderWindow_close(window);
+            }
+        }
 
+        sfRenderWindow_clear(window, (sfColor){100, 100, 100, 255});
+        sfRenderWindow_display(window);
+        
     }
 
     sfRenderWindow_destroy(window);
+
+
     return 0;
 }
